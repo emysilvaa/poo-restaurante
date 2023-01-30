f68e46a396192f1fa7a814b3a8bf1a12e1387a3e
Emilly Estefanny <emilly.e@escolar.ifrn.edu.br>
2023-01-30 10:37:11 -0300

    Atualizando main.py
    Colocando uma função no main.py para verificar se já existe o código do produto

--- codigo/main.py
+++ codigo/main.py
@@ -2,6 +2,17 @@ from produto import Produto
 from menu import Menu
 from funcoes import arquivoBebidas, arquivoPratos, arquivoSobremesas
 
+def verificaCodigo(arquivo, codigo):
+    existe = False
+    with open(arquivo , 'r') as f:
+        pratos = f.readlines()
+        for c in pratos:
+            linha = c.split('::')
+            if linha[0] == codigo:
+                print('já existe um produto cadastrado neste código! ')
+                existe = True
+                break
+    return existe
 
 while True:
     opcao = int(input('DIGITE UMA OPÇÃO\n1 - ENTRAR NO MENU\n2 - ADICIONAR PRODUTO\n3 - SAIR DO PROGRAMA\n --> '))
@@ -10,7 +21,7 @@ while True:
 
     elif opcao == 2:
         resposta2 = 0
-        while  resposta2 != 4 :
+        while  resposta2 != 4:
             print('1 - Pratos\n2 - Bebidas\n3 - Sobremesas')
             tipoProduto = int(input('--> '))
             codigo = str(input('Codigo: '))
@@ -19,20 +30,27 @@ while True:
 
             if tipoProduto == 1:
                 arquivoPratos()
-                with open('pratos.txt', 'a') as f:
-                    f.write(f'{codigo} :: {nomeProduto} :: {precoProduto} ::\n')
-
+                veri = verificaCodigo('pratos.txt', codigo)
+                if veri == False:      
+                    with open('pratos.txt', 'a') as f:
+                        f.write(f'{codigo}::{nomeProduto}::{precoProduto}::\n')
+               
                 
             elif tipoProduto == 2:
                 arquivoBebidas()
-                with open('bebidas.txt', 'a') as f:
-                    f.write(f'{codigo} :: {nomeProduto} :: {precoProduto} ::\n')
+                veri = verificaCodigo('bebidas.txt', codigo)
+                if veri == False:      
+                    with open('bebidas.txt', 'a') as f:
+                        f.write(f'{codigo}::{nomeProduto}::{precoProduto}::\n')
+                
 
-            else:
+            elif tipoProduto == 3:
                 arquivoSobremesas()
-                with open('sobremesas.txt', 'a') as f:
-                    f.write(f'{codigo} :: {nomeProduto} :: {precoProduto} ::\n')
-
+                veri = verificaCodigo('sobremesas.txt', codigo)
+                if veri == False:      
+                    with open('sobremesas.txt', 'a') as f:
+                        f.write(f'{codigo}::{nomeProduto}::{precoProduto}::\n')
+                
             resposta2 = int(input('Deseja adicionar outro produto?\n SIM: 3 NÃO: 4 > '))
             produto = Produto(codigo, nomeProduto, precoProduto)
             
