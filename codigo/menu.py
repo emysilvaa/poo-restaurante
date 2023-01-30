9faca1358de282eeb0c824d25f7b2dd49f4ccdea
jackson1002 <102551009+jackson1002@users.noreply.github.com>
2023-01-30 11:27:43 -0300

    Atualizando menu

--- codigo/menu.py
+++ codigo/menu.py
@@ -7,12 +7,42 @@ except:
 
 class Menu:
     def __init__(self):
-        with open('estoque.txt', 'r') as f:
-            estoque = f.readlines()
-
-        try:
-            existe = estoque[0]
-        except:
-            print('Estoque está vazio')
+        print('Você deseja ver o menu?')
+        resposta = input('Digite 1 para SIM e 2 para NÃO: ')
+        if resposta == '1':
+            with open('pratos.txt','r') as f:
+                pratos = f.readlines()
         
-        print(estoque)
+            with open('bebidas.txt','r') as f:
+                bebidas = f.readlines()
+        
+            with open('sobremesas.txt','r') as f:
+                sobremesas = f.readlines()
+        
+            with open('menu.txt','a') as f:
+                f.write('PRATOS:\n')
+                for c in pratos:
+                    if c:
+                        f.write(c)
+                f.write('BEBIDAS:\n')
+                for c in bebidas:
+                    if c:
+                        f.write(c)
+                f.write('SOBREMESAS:\n')
+                for c in sobremesas:
+                    if c:
+                         f.write(c)
+                print('PRATOS:')
+                for c in pratos:
+                    if c:
+                        print(c)
+                print('BEBIDAS:')
+                for c in bebidas:
+                    if c:
+                        print(c)
+                print('SOBREMESAS:')
+                for c in sobremesas:
+                    if c:
+                        print(c)
+        elif resposta == '2':
+            print('Você saiu do menu')
