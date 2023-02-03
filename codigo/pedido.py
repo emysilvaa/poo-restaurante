cf020d6611f5b4eb196aa66131a4ab6c5b1fc7a3
Emilly Estefanny <emilly.e@escolar.ifrn.edu.br>
2023-02-03 15:26:09 -0300

    Update pedido.py

--- codigo/pedido.py
+++ codigo/pedido.py
@@ -1,11 +1,3 @@
-from menu import Menu
-try:
-    with open('pedido.txt','r') as f:
-        pedido = f.readlines()
-except:
-    with open('pedido.txt','w') as f:
-        f.write('')
-
 class Pedido():
     def __init__(self, mesa, nomeCliente, codigoProduto, quantidade):
         self.mesa = mesa
